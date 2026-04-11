5579c34ff5b06a06b27d17b9102ac43b5a8d620b
RockBlack <98517320+RockBlack-VPN@users.noreply.github.com>
2026-04-11 12:08:56 +0300

    Create dreame.bat

---
+++ Global/Dreame/dreame.bat
@@ -0,0 +1,22 @@
+#AS16509 (Amazon Data Services)
+route add 3.120.0.0 mask 255.252.0.0 0.0.0.0
+route add 18.184.0.0 mask 255.254.0.0 0.0.0.0
+route add 18.192.0.0 mask 255.240.0.0 0.0.0.0
+route add 35.156.0.0 mask 255.252.0.0 0.0.0.0
+route add 52.28.0.0 mask 255.252.0.0 0.0.0.0
+route add 52.57.0.0 mask 255.255.0.0 0.0.0.0
+route add 54.93.0.0 mask 255.255.0.0 0.0.0.0
+
+#AS37963 (Alibaba Cloud / Hangzhou Alibaba)
+route add 47.91.0.0 mask 255.255.0.0 0.0.0.0
+route add 47.254.0.0 mask 255.254.0.0 0.0.0.0
+route add 47.74.0.0 mask 255.254.0.0 0.0.0.0
+route add 161.117.0.0 mask 255.255.0.0 0.0.0.0
+
+#AS15133 (MCI Communications / Verizon - Edge/CDN)
+route add 192.16.0.0 mask 255.240.0.0 0.0.0.0
+
+#AS8075 (Microsoft / Azure)
+route add 13.64.0.0 mask 255.192.0.0 0.0.0.0
+route add 40.64.0.0 mask 255.192.0.0 0.0.0.0
+route add 52.136.0.0 mask 255.248.0.0 0.0.0.0
